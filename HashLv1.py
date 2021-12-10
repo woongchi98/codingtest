0d2386b3e7a2421f5edc7f050dc7a784fe124c39
woongchi98 <86776972+woongchi98@users.noreply.github.com>
2021-12-10 12:06:02 +0900

    Question1.
    수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
    
    마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
    
    제한사항
    마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
    completion의 길이는 participant의 길이보다 1 작습니다.
    참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
    참가자 중에는 동명이인이 있을 수 있습니다.
    입출력 예
    participant	completion	return
    ["leo", "kiki", "eden"]	["eden", "kiki"]	"leo"
    ["marina", "josipa", "nikola", "vinko", "filipa"]	["josipa", "filipa", "marina", "nikola"]	"vinko"
    ["mislav", "stanko", "mislav", "ana"]	["stanko", "ana", "mislav"]	"mislav"
    입출력 예 설명
    예제 #1
    "leo"는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
    
    예제 #2
    "vinko"는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
    
    예제 #3
    "mislav"는 참여자 명단에는 두 명이 있지만, 완주자 명단에는 한 명밖에 없기 때문에 한명은 완주하지 못했습니다.

--- HashLv1.py
+++ HashLv1.py
@@ -1,8 +1,43 @@
-for part in participant: 
-    for comp in completion:
-        if part in completion:
-            participant.remove(comp)
+def solution(participant, completion):
+    for i in participant: 
+        if i in completion:
+            participant.remove(i)
+    return(participant)
+# 이론상으로는 맞는것 같은데 계속 생각과는 다르게 실행된다. 왜 그럴까?
+# 해결 못함
 
-answer = participant[0]
-#리스트를 사용 할 때는 크기를 조심해야한다.: 정수데이터는 int로 설정 하는 등
-#용량초과로 실패
+def solution(participant, completion):
+    for part in participant: 
+        for comp in completion:
+            if part in completion:
+                participant.remove(comp)
+
+    answer = participant[0]
+    return(answer)
+# 리스트를 사용 할 때는 크기를 조심해야한다.: 정수데이터는 int로 설정 하는 등
+# 용량초과로 실패
+# 용량이 적게 드는 딕셔너리를 이용해야 한다.
+
+def solution(participant, completion):
+    data = dict()
+    data['part'] = participant
+    data['compl']= completion
+## 이런식으로 dictionary를 만들려고 했으나, 딕셔너리는 중복이 안된다.
+## 그래서 각 인원을 세는 dictionary를 생성한다.
+
+def solution(participant, completion):
+    answer = {}
+    
+    #participant를 카운트해주는 answer를 만든다.
+    for i in participant:
+        answer[i] = answer.get(i, 0) + 1
+    #값을 빼준다
+    for j in completion:
+        answer[j] -= 1
+    #
+    for k in answer:
+        if answer[k] : 
+            return k
+
+# get 함수를 사용하였다
+# dictionary.get(a,b) : key 'a'의 value 반환, 없으면 b를 반환
