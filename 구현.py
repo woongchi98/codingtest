4267fd080234d015df9438b41a188588a937140d
Chiwoong Moon <86776972+woongchi98@users.noreply.github.com>
2021-12-17 14:34:36 +0900

    Update 구현.py

--- 구현.py
+++ 구현.py
@@ -39,3 +39,41 @@ def solution(N, direction):
 
     answers = now
     return  answers
+   
+   '''
+   return 값이 리스트 형식이다
+   리스트 형식을 어떻게 바꾸지?
+   (now[0], now[1]) 이렇게 출력하면 되겠다.
+   '''
+   
+   
+#풀이 2
+def solution(N, direction):
+    x, y = 1, 1
+
+    move_typaes = ['L','R','D','U']
+    dx = [0,0,-1,1]
+    dy = [-1,1,0,0]
+    
+    nx = 0
+    ny = 0
+    
+    for i in direction :
+        for j in range(len(move_types)):
+            if i == move_types[j] :
+                nx = x + dx[j]
+                ny = y + dy[j]
+            
+        if nx < 1 or ny < 1 or nx > N or ny > N:
+            continue
+            
+        x, y = nx, ny
+    return (x,y)
+
+   
+'''
+여기서 continue 와 pass의 차이를 꺠달음.
+pass : 시행할 것이 없다는 의미
+continue : 반복문 처음으로 돌아감.
+위 예제에서는 nx나 ny가 범위를 벗어나면 다음 시행을 하는것.
+'''
